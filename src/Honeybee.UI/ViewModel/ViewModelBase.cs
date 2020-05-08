240b5dff9a1f325ee8e6a96c6e396764be800caa
MingboPeng <mingo1214@gmail.com>
2020-05-08 18:56:27 -0400

    feat(programType): proof of concept for program type's UI

---
+++ src/Honeybee.UI/ViewModel/ViewModelBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Honeybee.UI
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        //internal void Set<T>(ref T field, T newValue) where T: class 
+        //{
+        //    if (field != newValue)
+        //    {
+        //        field = newValue;
+        //        OnPropertyChanged();
+        //    }
+        //}
+        internal void Set(Action setAction, string memberName)
+        {
+            setAction();
+            OnPropertyChanged(memberName);
+        }
+        void OnPropertyChanged([CallerMemberName] string memberName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(memberName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //public void RefreshControl(string memberName)
+        //{
+        //    OnPropertyChanged(memberName);
+        //}
+        public void RefreshControls(IEnumerable<string> memberNames)
+        {
+            foreach (var item in memberNames)
+            {
+                OnPropertyChanged(item);
+            }
+            
+        }
+    }
+
+
+
+}
