ed654e2ccaf56563dafead358b2708028e0238c7
MingboPeng <mingo1214@gmail.com>
2020-05-12 14:57:12 -0400

    refactor(schedule): add a layout class for schedule

---
+++ src/Honeybee.UI/Layout/Schedule.cs
@@ -0,0 +1,489 @@
+﻿using Eto.Drawing;
+using Eto.Forms;
+using HoneybeeSchema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Honeybee.UI
+{
+    public class Panel_Schedule: DynamicLayout
+    {
+        private double _intervalMinutes = 60; // hourly = 60, 15 minutes = 15, etc
+
+        public Panel_Schedule()
+        {
+            //var layout = new DynamicLayout();
+            this.DefaultPadding = new Padding(10);
+            this.DefaultSpacing = new Size(5, 5);
+
+            //var datePicker = new DateTimePicker();
+            //layout.AddRow(datePicker);
+            //layout.AddRow(null);
+
+            var dayValues = new List<double>() { 0.16, 0.5, 0 };
+            var dayTimes = new List<(int hour, int minute)>() { (0, 0), (6, 0), (18, 0) };
+            var hours = dayTimes.Select(_ => _.hour).ToArray();
+
+
+            var mouseHoverValue_TB = new NumericMaskedTextBox<double>() { PlaceholderText = "Mouse over lines" };
+            var label = new Label() { Text = "" };
+            this.AddSeparateRow(null, label, mouseHoverValue_TB);
+
+
+            var drawable = new Drawable(true)
+            {
+                Size = new Size(600, 400),
+                BackgroundColor = Colors.White
+            };
+            var location = new Point(0, 0);
+            var canvas = drawable.Bounds;
+            canvas.BottomRight = new Point(canvas.Width - 10, canvas.Height - 10);
+            canvas.TopLeft = new Point(10, 10);
+
+
+
+
+            //var drawableTarget = new DrawableTarget(drawable) { UseOffScreenBitmap = true };
+            EventHandler<MouseEventArgs> mouseHandler = (s, e) => {
+                location = new Point(e.Location);
+                ((Control)s).Invalidate();
+                e.Handled = true;
+            };
+            //drawable.MouseMove += mouseHandler;
+            //drawable.MouseDoubleClick += mouseHandler;
+
+            var allMouseHoverRanges = new List<(bool isVertical, RectangleF rectangle, int valueIndex)>();
+            var mouseHoveredRanges = new List<(bool isVertical, RectangleF rectangle, int valueIndex)>();
+            drawable.MouseMove += (s, e) =>
+            {
+                var mouseLoc = e.Location;
+                //Draw mouse hover over ranges
+                var hovered = allMouseHoverRanges.Where(_ => _.rectangle.Contains(mouseLoc));
+                if (hovered.Any())
+                {
+                    mouseHoveredRanges = hovered.ToList();
+                    drawable.Update(drawable.Bounds);
+                }
+                else
+                {
+                    if (mouseHoveredRanges.Any())
+                    {
+                        var preRec = mouseHoveredRanges.First().rectangle;
+                        mouseHoveredRanges.Clear();
+                        drawable.Update(new Rectangle(preRec));
+                    }
+
+                }
+
+            };
+
+            var startDragging = false;
+            var mouseHoveredRangesForDragging = new List<(bool isVertical, RectangleF rectangle, int valueIndex)>();
+            drawable.MouseDown += (s, e) =>
+            {
+                if (mouseHoveredRanges.Any())
+                {
+                    startDragging = e.Buttons == MouseButtons.Primary;
+                    mouseHoveredRangesForDragging = mouseHoveredRanges;
+                    //label.Text = startDragging.ToString();
+                }
+
+            };
+            drawable.MouseUp += (s, e) =>
+            {
+                //if (mouseHoveredRangesForDragging.Any())
+                //{
+
+
+                //}
+                if (startDragging)
+                {
+                    mouseHoveredRangesForDragging.Clear();
+                    startDragging = false;
+                    drawable.Update(drawable.Bounds);
+                }
+
+
+            };
+            // mouse move for dragging
+            drawable.MouseMove += (s, e) =>
+            {
+                if (!startDragging)
+                    return;
+
+                var mouseLoc = e.Location;
+                if (canvas.Contains(new Point(mouseLoc)) && mouseHoveredRangesForDragging.Any())
+                {
+                    var hovered = mouseHoveredRangesForDragging.First();
+                    var valueIndex = hovered.valueIndex;
+                    //TODO: need to do conversion based on schedule's max and min. For now, it just calculated %.
+                    //var mappedValue = (canvas.Height - (mouseLoc - canvas.Location).Y) / canvas.Height;
+                    var mappedValue = (canvas.Bottom - mouseLoc.Y) / canvas.Height;
+                    var pecent = Math.Round(mappedValue, 3);
+
+                    //label.Text = $"{canvas.Bottom} - {mouseLoc.Y} / {canvas.Height} = {pecent}";
+                    dayValues[valueIndex] = pecent;
+                    drawable.Update(drawable.Bounds);
+                }
+                else
+                {
+                    mouseHoveredRangesForDragging.Clear();
+                    startDragging = false;
+                }
+
+
+
+            };
+
+
+            drawable.MouseDoubleClick += (s, e) =>
+            {
+                if (mouseHoveredRanges.Any())
+                {
+                    var hovered = mouseHoveredRanges.First();
+                    if (hovered.isVertical)
+                    {
+                        dayTimes.RemoveAt(hovered.valueIndex);
+                        dayValues.RemoveAt(hovered.valueIndex);
+                        mouseHoveredRanges.Clear();
+                        drawable.Update(drawable.Bounds);
+
+                        return;
+                    }
+
+                    var mouseLoc = e.Location;
+                    //TODO: need to do conversion based on schedule's max and min. For now, it just calculated %.
+                    var mappedTimeRaw = (mouseLoc.X - canvas.Left) / canvas.Width * 24;
+                    var mappedHour = (int)mappedTimeRaw;
+                    var mappedMinute = (int)NormalizeMinute((int)(Math.Abs(mappedTimeRaw - mappedHour) * 60));
+                    if (mappedMinute == 60)
+                    {
+                        mappedHour++;
+                        mappedMinute = 0;
+                    }
+                    var mappedTimeNormalized = mappedHour + (double)(mappedMinute / 60.0);
+
+                    var newDateTimes = dayTimes.Select(_ => _.hour + (double)(_.minute / 60.0)).ToList();
+                    if (!newDateTimes.Contains(mappedTimeNormalized))
+                    {
+                        newDateTimes.Add(mappedTimeNormalized);
+                    }
+                    newDateTimes.Sort();
+                    var foundIndex = newDateTimes.IndexOf(mappedTimeNormalized);
+
+                    var insertIndex = Math.Max(0, foundIndex - 1);
+                    dayTimes.Insert(foundIndex, (mappedHour, mappedMinute));
+                    var addValue = dayValues[insertIndex];
+                    dayValues.Insert(insertIndex, addValue);
+
+                    drawable.Update(drawable.Bounds);
+                }
+
+            };
+
+            var hoveredValueIndex = 0;
+            drawable.Paint += (s, e) =>
+            {
+                //var path = new GraphicsPath();
+                //path.AddEllipse(25, 25, 50, 50);
+                //path.AddRectangle(125, 25, 50, 50);
+                //path.AddLines(new PointF(225, 25), new PointF(225, 75), new PointF(275, 50));
+                //path.CloseFigure();
+                var mouseLoc = location;
+
+                var graphics = e.Graphics;
+
+                //Draw schedule
+
+                var hourPts = GenHourPts(dayValues, dayTimes, canvas);
+                var graphElements = GenPts(hourPts, canvas);
+                var allPts = graphElements.points;
+                allMouseHoverRanges = graphElements.ranges;
+
+                //graphics(Colors.Black, canvas);
+                graphics.DrawRectangle(Colors.Black, canvas);
+
+
+                //Draw mouse hover over ranges
+                //var hovered = allMouseHoverRanges.Where(_ => _.rectangle.Contains(mouseLoc));
+                var hovered = mouseHoveredRanges;
+                if (hovered.Any())
+                {
+                    //mouseHoverValue_TB.Text = null;
+                    //label.Text = null;
+
+                    var hoveredRec = hovered.First();
+                    var rec = hoveredRec.rectangle;
+                    //draw hover rec
+                    graphics.FillRectangle(Color.FromArgb(200, 200, 200), rec);
+                    //draw text
+                    var textLoc = hoveredRec.rectangle.Center;
+                    var font = Fonts.Sans(10);
+                    var valueToDisplay = string.Empty;
+                    if (hoveredRec.isVertical)
+                    {
+                        var time = dayTimes[hoveredRec.valueIndex];
+
+                        valueToDisplay = TimeSpan.Parse($"{time.hour}:{time.minute}").ToString(@"hh\:mm");
+                        valueToDisplay = $" {valueToDisplay}";
+                    }
+                    else
+                    {
+                        valueToDisplay = dayValues[hoveredRec.valueIndex].ToString();
+                    }
+
+                    var textSize = font.MeasureString(valueToDisplay);
+                    graphics.DrawText(font, Colors.Black, textLoc.X - textSize.Width / 2, textLoc.Y - textSize.Height / 2 - 8, valueToDisplay);
+                    hoveredValueIndex = hoveredRec.valueIndex;
+                    if (!mouseHoverValue_TB.HasFocus)
+                        mouseHoverValue_TB.Focus();
+
+                    var hoveredValue = dayValues[hoveredRec.valueIndex];
+                    if (mouseHoverValue_TB.SelectedText != hoveredValue.ToString())
+                    {
+                        mouseHoverValue_TB.Value = hoveredValue;
+                        mouseHoverValue_TB.SelectAll();
+                    }
+
+                }
+
+                foreach (var pt in allPts)
+                {
+                    graphics.FillRectangle(Colors.Black, new RectangleF(pt.X - 3, pt.Y - 3, 6, 6));
+                }
+
+                var pen = new Pen(Colors.Black, 2);
+                graphics.DrawLines(pen, allPts);
+
+
+
+                //graphics.FillRectangle(Colors.Black, new Rectangle((int)hourX, (int)hourY, 2, 2));
+
+                //var loc = location;
+                //loc.Restrict(new Rectangle(image.Size));
+
+                //graphics.FillRectangle(Colors.Black, new Rectangle(loc.X - 50, loc.Y - 50, 100, 100));
+                //drawableTarget.EndDraw(e);
+            };
+
+            mouseHoverValue_TB.KeyDown += (s, e) => {
+                if (e.Key == Keys.Enter)
+                {
+                    if (mouseHoveredRanges.Any())
+                    {
+                        var preRec = mouseHoveredRanges.First().rectangle;
+                        mouseHoveredRanges.Clear();
+
+                        var valueIndex = hoveredValueIndex;
+                        var newUserInput = mouseHoverValue_TB.Value;
+                        var oldValue = dayValues[valueIndex];
+
+                        //preRec.Top = (float) Math.Max(newUserInput, oldValue);
+                        //preRec.Bottom = (float)Math.Min(newUserInput, oldValue);
+                        dayValues[valueIndex] = newUserInput;
+
+                        drawable.Update(drawable.Bounds);
+                        //drawable.Update(new Rectangle(preRec));
+                    }
+
+
+                    //drawable.Update(drawable.Bounds);
+                }
+
+            };
+
+            //drawable.Paint += (s, e) => {
+
+            //    var graphics = e.Graphics;
+
+            //    var loc = location;
+            //    //loc.Restrict(new Rectangle(image.Size));
+            //    graphics.FillRectangle(Colors.Black, new Rectangle(loc.X-50, loc.Y-50, 100, 100));
+            //    //drawableTarget.EndDraw(e);
+            //};
+
+            //control.Paint += delegate (object sender, PaintEventArgs pe) {
+            //    pe.Graphics.FillRectangle(Colors.Black, new Rectangle(150, 150, 100, 100));
+            //    var inc = 400;
+            //    for (int i = 0; i <= control.Size.Width / inc; i++)
+            //    {
+            //        var pos = i * inc;
+            //        pe.Graphics.DrawLine(Colors.White, new Point(pos, 0), new Point(pos + control.Size.Width, control.Size.Height));
+            //        pe.Graphics.DrawLine(Colors.White, new Point(pos, 0), new Point(pos - control.Size.Width, control.Size.Height));
+            //    }
+            //    var lpos = 100;
+            //    pe.Graphics.DrawLine(Colors.White, new Point(0, lpos), new Point(control.Size.Width, lpos));
+            //    pe.Graphics.DrawLine(Colors.White, new Point(lpos, 0), new Point(lpos, control.Size.Height));
+
+            //};
+            this.AddRow(drawable);
+
+        }
+ 
+
+        private double NormalizeMinute(int oldMinute)
+        {
+            var checkedMinute = Math.Round(oldMinute / _intervalMinutes) * _intervalMinutes;
+            return checkedMinute;
+        }
+        private List<PointF> GenHourPts(List<double> dayValues, List<(int hour, int minute)> dayTimes, RectangleF frameBound)
+        {
+            var canv = frameBound;
+            var widthPerHour = canv.Width / 24;
+            var heightPerHour = canv.Height;
+            var count = dayValues.Count();
+            var hourPts = new List<PointF>();
+            for (int i = 0; i < count; i++)
+            {
+                var checkedMinute = NormalizeMinute(dayTimes[i].minute);
+                var time = (double)dayTimes[i].hour + checkedMinute / 60;
+                var hourX = canv.Left + time * widthPerHour;
+                var hourY = (canv.Bottom - (float)dayValues[i] * heightPerHour);
+                hourPts.Add(new PointF((float)hourX, hourY));
+            }
+
+            return hourPts;
+        }
+
+        private (List<PointF> points, List<(bool isVertical, RectangleF rectangle, int valueIndex)> ranges) GenPts(List<PointF> hourPts, RectangleF frameBound)
+        {
+            var count = hourPts.Count;
+            var pts = new List<PointF>();
+            var recs = new List<(bool, RectangleF, int)>();
+            for (int i = 0; i < count; i++)
+            {
+                var pt = hourPts[i];
+
+                if (i == 0)
+                {
+                    //TODO: need to add a check if pt.X is 0
+                    if (pt.Y != frameBound.Bottom)
+                    {
+                        // | p1 ------ p2
+                        // |                  
+                        // |                   
+                        // |_0__________________24_
+
+                        //Begining of the schedule 
+                        var p1 = new PointF(frameBound.BottomLeft.X, pt.Y);
+                        var p2 = new PointF(pt.X, pt.Y);
+                        pts.Add(p1);
+                        pts.Add(p2);
+
+                        var rec = new RectangleF() { Location = p1, EndLocation = p2 };
+                        rec.Top = rec.Top - 10;
+                        rec.Bottom = rec.Bottom + 10;
+                        recs.Add((false, rec, i));
+                    }
+                    else
+                    {
+                        // | 
+                        // |                  
+                        // |                   
+                        // |_pt____________________
+
+                        pts.Add(pt);
+                    }
+
+                    if (count == 1)
+                    {
+                        // | pt ----------------pt2
+                        // |                  
+                        // |                   
+                        // |_0__________________24_
+                        var pt2 = new PointF(frameBound.Right, pt.Y);
+                        pts.Add(pt);
+                        pts.Add(pt2);
+                        // Horizontal 
+                        var rec2 = new RectangleF() { Location = pt, EndLocation = pt2 };
+                        rec2.Top = rec2.Top - 10;
+                        rec2.Bottom = rec2.Bottom + 10;
+                        recs.Add((false, rec2, i));
+
+
+                    }
+
+                }
+                else if (i == count - 1)
+                {
+                    //  prePt0 ------ prePt1
+                    //                   |
+                    //                   |
+                    //                   pt------ pt2(hour == 24)
+
+                    var prePt0 = hourPts[i - 1];
+                    var prePt1 = new PointF(pt.X, prePt0.Y);
+                    pts.Add(prePt1);
+                    // Horizontal 
+                    var rec = new RectangleF() { Location = prePt0, EndLocation = prePt1 };
+                    rec.Top = rec.Top - 10;
+                    rec.Bottom = rec.Bottom + 10;
+                    recs.Add((false, rec, i - 1));
+
+                    // Vertical
+                    var lowerPt = new PointF(prePt1.X, Math.Min(prePt1.Y, pt.Y));
+                    var higherPt = new PointF(prePt1.X, Math.Max(prePt1.Y, pt.Y));
+                    var rec1 = new RectangleF() { Location = lowerPt, EndLocation = higherPt };
+                    rec1.Left = rec1.Left - 10;
+                    rec1.Right = rec1.Right + 10;
+                    rec1.Top = rec1.Top - 20;
+                    rec1.Bottom = rec1.Bottom + 20;
+                    //rec1.TopLeft = new PointF(rec1.TopLeft.X - 10, rec1.TopLeft.Y - 10);
+                    //rec1.BottomRight = new PointF(rec1.BottomRight.X + 10, rec1.BottomRight.Y + 10);
+                    recs.Add((true, rec1, i));
+
+
+                    var pt2 = new PointF(frameBound.Right, pt.Y);
+                    pts.Add(pt);
+                    pts.Add(pt2);
+                    // Horizontal 
+                    var rec2 = new RectangleF() { Location = pt, EndLocation = pt2 };
+                    rec2.Top = rec2.Top - 10;
+                    rec2.Bottom = rec2.Bottom + 10;
+                    recs.Add((false, rec2, i));
+
+                }
+                else
+                {
+                    //              (OR) pt
+                    //                  |
+                    //                  |
+                    //  prePt0------ prePt1
+                    //                  |
+                    //                  |
+                    //                  pt
+
+                    var prePt0 = hourPts[i - 1];
+                    var prePt1 = new PointF(pt.X, prePt0.Y);
+                    pts.Add(prePt1);
+                    pts.Add(pt);
+
+                    // Horizontal 
+                    var rec = new RectangleF() { Location = prePt0, EndLocation = prePt1 };
+                    rec.Top = rec.Top - 10;
+                    rec.Bottom = rec.Bottom + 10;
+                    recs.Add((false, rec, i - 1));
+
+                    // Vertical
+                    var lowerPt = new PointF(prePt1.X, Math.Min(prePt1.Y, pt.Y));
+                    var higherPt = new PointF(prePt1.X, Math.Max(prePt1.Y, pt.Y));
+                    var rec1 = new RectangleF() { Location = lowerPt, EndLocation = higherPt };
+                    rec1.Left = rec1.Left - 10;
+                    rec1.Right = rec1.Right + 10;
+                    rec1.Top = rec1.Top - 20;
+                    rec1.Bottom = rec1.Bottom + 20;
+                    //rec1.BottomRight = new PointF(rec1.BottomRight.X + 10, rec1.BottomRight.Y + 10);
+                    recs.Add((true, rec1, i));
+
+
+                }
+            }
+
+            return (pts, recs);
+        }
+
+
+
+    }
+}
