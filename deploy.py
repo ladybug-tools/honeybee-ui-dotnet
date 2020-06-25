2a23dabee817c671178a1e894e177793415a65c1
MingboPeng <mingo1214@gmail.com>
2020-06-25 16:04:58 -0400

    fix(deploy): added nuspec and separate Honeybee.UI.Rhino

--- deploy.py
+++ deploy.py
@@ -36,12 +36,11 @@ try:
     nuget_push = f"nuget push Honeybee.UI.{BUILD_VERSION}.nupkg -ApiKey {NUGET_API_KEY} -Source https://api.nuget.org/v3/index.json"
     subprocess.check_output(nuget_push)
 
-    nuget_pack = f"nuget pack src/Honeybee.UI/Honeybee.UI.csproj -suffix Rhino -Properties Configuration=Release_Rhino -Version {BUILD_VERSION} -Verbosity detailed"
+    nuget_pack = f"nuget pack src/Honeybee.UI/Honeybee.UI.csproj -Properties Configuration=Release_Rhino -Properties id=Honeybee.UI.Rhino -Version {BUILD_VERSION} -Verbosity detailed"
     subprocess.check_output(nuget_pack)
-    nuget_push = f"nuget push Honeybee.UI.{BUILD_VERSION}-Rhino.nupkg -ApiKey {NUGET_API_KEY} -Source https://api.nuget.org/v3/index.json"
+    nuget_push = f"nuget push Honeybee.UI.Rhino.{BUILD_VERSION}.nupkg -ApiKey {NUGET_API_KEY} -Source https://api.nuget.org/v3/index.json"
     subprocess.check_output(nuget_push)
 
 except Exception as e:
     print(str(e))
     raise Exception(e)
-
