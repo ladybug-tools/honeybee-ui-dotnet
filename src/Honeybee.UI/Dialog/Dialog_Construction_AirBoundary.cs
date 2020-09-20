b9df0458bb15b75df0722438a31a5fb1f0acbbfc
MingboPeng <mingo1214@gmail.com>
2020-09-20 13:43:20 -0400

    feat(Construction): add a dialog for AirBoundaryConstruction and ShadeConstruction

---
+++ src/Honeybee.UI/Dialog/Dialog_Construction_AirBoundary.cs
@@ -0,0 +1,69 @@
+﻿using Eto.Drawing;
+using Eto.Forms;
+using HB = HoneybeeSchema;
+
+namespace Honeybee.UI
+{
+
+    public class Dialog_Construction_AirBoundary : Dialog<HB.AirBoundaryConstructionAbridged>
+    {
+        public Dialog_Construction_AirBoundary(HB.AirBoundaryConstructionAbridged airBoundaryConst)
+        {
+            var _hbObj = airBoundaryConst;
+
+            Padding = new Padding(10);
+            Resizable = true;
+            Title = "Material - Honeybee";
+            WindowStyle = WindowStyle.Default;
+            Width = 450;
+            this.Icon = DialogHelper.HoneybeeIcon;
+
+            var OkButton = new Button { Text = "OK" };
+            OkButton.Click += (sender, e) => Close(_hbObj);
+
+            AbortButton = new Button { Text = "Cancel" };
+            AbortButton.Click += (sender, e) => Close();
+
+
+            var layout = new DynamicLayout();
+            layout.DefaultSpacing = new Size(5, 5);
+            layout.DefaultPadding = new Padding(10, 5);
+
+            layout.AddRow("Name");
+            var name = new TextBox();
+            _hbObj.DisplayName = _hbObj.DisplayName ?? _hbObj.Identifier;
+            name.TextBinding.Bind(() => _hbObj.DisplayName, (v) => _hbObj.DisplayName = v);
+            layout.AddRow(name);
+
+
+            //AirMixingPerArea
+            var airMx = new MaskedTextBox();
+            airMx.Provider = new NumericMaskedTextProvider() { AllowDecimal = true };
+            airMx.TextBinding.Bind(() => _hbObj.AirMixingPerArea.ToString(), (v) => _hbObj.AirMixingPerArea = double.Parse(v));
+            layout.AddRow(nameof(_hbObj.AirMixingPerArea));
+            layout.AddRow(airMx);
+
+            //AirMixingSchedule
+            var airMxSch = new TextBox() { Enabled = false};
+            airMxSch.TextBinding.Bind(() => _hbObj.AirMixingSchedule, (v) => _hbObj.AirMixingSchedule = v);
+            layout.AddRow(nameof(_hbObj.AirMixingSchedule));
+            layout.AddRow(airMxSch);
+
+            var buttonSource = new Button { Text = "HBData" };
+            buttonSource.Click += (s, e) =>
+            {
+                Dialog_Message.Show(this, _hbObj.ToJson());
+            };
+
+
+            layout.AddRow(null);
+            layout.AddRow(buttonSource);
+            layout.AddRow(null);
+            layout.AddSeparateRow(null, OkButton, AbortButton, null);
+
+            Content = layout;
+
+        }
+
+    }
+}
---
+++ src/Honeybee.UI/Dialog/Dialog_Construction_Shade.cs
@@ -0,0 +1,77 @@
+﻿using Eto.Drawing;
+using Eto.Forms;
+using HB = HoneybeeSchema;
+
+namespace Honeybee.UI
+{
+
+    public class Dialog_Construction_Shade : Dialog<HB.ShadeConstruction>
+    {
+        public Dialog_Construction_Shade(HB.ShadeConstruction shadeConstruction)
+        {
+            var _hbObj = shadeConstruction;
+
+            Padding = new Padding(10);
+            Resizable = true;
+            Title = "Material - Honeybee";
+            WindowStyle = WindowStyle.Default;
+            //MinimumSize = new Size(450, 250);
+            Width = 450;
+            this.Icon = DialogHelper.HoneybeeIcon;
+
+            var OkButton = new Button { Text = "OK" };
+            OkButton.Click += (sender, e) => Close(_hbObj);
+
+            AbortButton = new Button { Text = "Cancel" };
+            AbortButton.Click += (sender, e) => Close();
+
+
+            var layout = new DynamicLayout();
+            layout.DefaultSpacing = new Size(5, 5);
+            layout.DefaultPadding = new Padding(10, 5);
+
+            layout.AddRow("Name");
+            var name = new TextBox();
+            _hbObj.DisplayName = _hbObj.DisplayName ?? _hbObj.Identifier;
+            name.TextBinding.Bind(() => _hbObj.DisplayName, (v) => _hbObj.DisplayName = v);
+            layout.AddRow(name);
+
+
+            //SolarReflectance
+            var solarRef = new MaskedTextBox();
+            solarRef.Provider = new NumericMaskedTextProvider() { AllowDecimal = true };
+            solarRef.TextBinding.Bind(() => _hbObj.SolarReflectance.ToString(), (v) => _hbObj.SolarReflectance = double.Parse(v));
+            layout.AddRow(nameof(_hbObj.SolarReflectance));
+            layout.AddRow(solarRef);
+
+            //VisibleReflectance
+            var visibleRef = new MaskedTextBox();
+            visibleRef.Provider = new NumericMaskedTextProvider() { AllowDecimal = true };
+            visibleRef.TextBinding.Bind(() => _hbObj.VisibleReflectance.ToString(), (v) => _hbObj.VisibleReflectance = double.Parse(v));
+            layout.AddRow(nameof(_hbObj.VisibleReflectance));
+            layout.AddRow(visibleRef);
+
+            // IsSpecular
+            var IsSpecular = new CheckBox();
+            IsSpecular.CheckedBinding.Bind(() => _hbObj.IsSpecular, (v) => _hbObj.IsSpecular = v.GetValueOrDefault(false));
+            layout.AddRow(nameof(_hbObj.IsSpecular));
+            layout.AddRow(IsSpecular);
+
+            var buttonSource = new Button { Text = "HBData" };
+            buttonSource.Click += (s, e) =>
+            {
+                Dialog_Message.Show(this, _hbObj.ToJson());
+            };
+
+
+            layout.AddRow(null);
+            layout.AddRow(buttonSource);
+            layout.AddRow(null);
+            layout.AddSeparateRow(null, OkButton, AbortButton, null);
+
+            Content = layout;
+
+        }
+
+    }
+}
