d1704c29a9bb38b361c80b375665eb02e90187f6
MingboPeng <mingo1214@gmail.com>
2021-06-01 13:24:39 -0400

    feat(RoomProperty): add CheckboxButtonViewModel, add loads

--- src/Honeybee.UI/Layout/RoomProperty.cs
+++ src/Honeybee.UI/Layout/RoomProperty.cs
@@ -73,8 +73,34 @@ namespace Honeybee.UI.View
             layout.DefaultSpacing = new Size(4, 4);
             layout.DefaultPadding = new Padding(4);
 
-       
-  
+            var tb = new TabControl();
+            
+            var basis = GenBasisPanel();
+            tb.Pages.Add(new TabPage(basis) { Text = "Basis" });
+
+            var loads = GenLoadsPanel();
+            //layout.AddSeparateRow(loads);
+            tb.Pages.Add(new TabPage(loads) { Text= "Override Room Program Loads"});
+
+            layout.AddRow(tb);
+
+            layout.AddRow(null);
+            var data_button = new Button { Text = "Schema Data" };
+            data_button.Command = this._vm.HBDataBtnClick;
+            layout.AddSeparateRow(data_button, null);
+
+
+            this.Content = layout;
+        }
+
+        private DynamicLayout GenBasisPanel()
+        {
+            var layout = new DynamicLayout() { Height = 350 };
+            var vm = this._vm;
+
+            layout.DefaultSpacing = new Size(4, 4);
+            layout.DefaultPadding = new Padding(4);
+
             var id = new Label();
             id.Width = 250;
             id.TextBinding.Bind(vm, (_) => _.Identifier);
@@ -93,40 +119,40 @@ namespace Honeybee.UI.View
             multiplier_NS.TextBinding.Bind(vm, _ => _.MultiplierText);
             layout.AddRow("Multiplier:", multiplier_NS);
 
+            layout.AddSpace();
 
             //Get modifierSet
             var modifierSetDP = new Button();
-            modifierSetDP.Width = 200;
-            modifierSetDP.Bind((t) => t.Enabled, vm, v => v.IsModifierSetBtnEnabled);
-            modifierSetDP.TextBinding.Bind(vm, _ => _.ModifierSetName);
+            modifierSetDP.Bind((t) => t.Enabled, vm, v => v.ModifierSet.IsBtnEnabled);
+            modifierSetDP.TextBinding.Bind(vm, _ => _.ModifierSet.BtnName);
             modifierSetDP.Command = vm.ModifierSetCommand;
             var modifierSetByProgram = new CheckBox() { Text = vm.ByGlobalModifierSet };
-            modifierSetByProgram.CheckedBinding.Bind(vm, _ => _.IsByGlobalModifierSet);
+            modifierSetByProgram.CheckedBinding.Bind(vm, _ => _.ModifierSet.IsCheckboxChecked);
 
             layout.AddRow("Modifier Set:", modifierSetByProgram);
             layout.AddRow(null, modifierSetDP);
 
+            layout.AddSpace();
 
             //Get constructions
             var constructionSetDP = new Button();
-            constructionSetDP.Width = 200;
-            constructionSetDP.Bind(_ => _.Enabled, vm, _ => _.IsCSetBtnEnabled);
-            constructionSetDP.TextBinding.Bind(vm, _ => _.ConstructionSetName);
+            constructionSetDP.Bind(_ => _.Enabled, vm, _ => _.ConstructionSet.IsBtnEnabled);
+            constructionSetDP.TextBinding.Bind(vm, _ => _.ConstructionSet.BtnName);
             constructionSetDP.Command = vm.RoomConstructionSetCommand;
             var cSetByProgram = new CheckBox() { Text = vm.ByGlobalConstructionSet };
-            cSetByProgram.CheckedBinding.Bind(vm, _ => _.IsByGlobalConstructionSet);
+            cSetByProgram.CheckedBinding.Bind(vm, _ => _.ConstructionSet.IsCheckboxChecked);
 
-            layout.AddRow("Construction Set:", cSetByProgram );
+            layout.AddRow("Construction Set:", cSetByProgram);
             layout.AddRow(null, constructionSetDP);
 
 
             //Get programs
             var programTypesDP = new Button();
-            programTypesDP.Bind((t) => t.Enabled, vm, v => v.IsPTypeBtnEnabled);
-            programTypesDP.TextBinding.Bind(vm, _ => _.ProgramTypeName);
+            programTypesDP.Bind((t) => t.Enabled, vm, v => v.PropgramType.IsBtnEnabled);
+            programTypesDP.TextBinding.Bind(vm, _ => _.PropgramType.BtnName);
             programTypesDP.Command = vm.RoomProgramTypeCommand;
             var pTypeByProgram = new CheckBox() { Text = vm.Unoccupied };
-            pTypeByProgram.CheckedBinding.Bind(vm, _ => _.IsUnoccupied);
+            pTypeByProgram.CheckedBinding.Bind(vm, _ => _.PropgramType.IsCheckboxChecked);
 
             layout.AddRow("Program Type:", pTypeByProgram);
             layout.AddRow(null, programTypesDP);
@@ -134,25 +160,128 @@ namespace Honeybee.UI.View
 
             //Get HVACs
             var hvacDP = new Button();
-            hvacDP.Bind((t) => t.Enabled, vm, v => v.IsHVACBtnEnabled);
-            hvacDP.TextBinding.Bind(vm, _ => _.HVACName);
+            hvacDP.Bind((t) => t.Enabled, vm, v => v.HVAC.IsBtnEnabled);
+            hvacDP.TextBinding.Bind(vm, _ => _.HVAC.BtnName);
             hvacDP.Command = vm.RoomHVACCommand;
             var hvacByProgram = new CheckBox() { Text = vm.Unconditioned };
-            hvacByProgram.CheckedBinding.Bind(vm, _ => _.IsUnconditioned);
+            hvacByProgram.CheckedBinding.Bind(vm, _ => _.HVAC.IsCheckboxChecked);
 
             layout.AddRow("HVAC:", hvacByProgram);
             layout.AddRow(null, hvacDP);
 
+            layout.AddRow(null);
+            return layout;
+        }
+
+        private DynamicLayout GenLoadsPanel()
+        {
+            var layout = new DynamicLayout() { Height = 350 };
+            var vm = this._vm;
+
+            layout.DefaultSpacing = new Size(4, 4);
+            layout.DefaultPadding = new Padding(4);
+            layout.BeginScrollable( BorderType.None);
+
+            //Get lighting
+            var ltnDP = new Button();
+            ltnDP.Width = 250;
+            ltnDP.Bind((t) => t.Enabled, vm, v => v.Lighting.IsBtnEnabled);
+            ltnDP.TextBinding.Bind(vm, _ => _.Lighting.BtnName);
+            ltnDP.Command = vm.RoomHVACCommand;
+            var ltnByProgram = new CheckBox() { Text = vm.ByProgramType };
+            ltnByProgram.CheckedBinding.Bind(vm, _ => _.Lighting.IsCheckboxChecked);
+
+            layout.AddRow("Lighting:", ltnByProgram);
+            layout.AddRow(null, ltnDP);
 
 
+            //Get People
+            var pplDP = new Button();
+            pplDP.Bind((t) => t.Enabled, vm, v => v.People.IsBtnEnabled);
+            pplDP.TextBinding.Bind(vm, _ => _.People.BtnName);
+            pplDP.Command = vm.RoomHVACCommand;
+            var pplByProgram = new CheckBox() { Text = vm.ByProgramType };
+            pplByProgram.CheckedBinding.Bind(vm, _ => _.People.IsCheckboxChecked);
 
-            layout.Add(null);
-            var data_button = new Button { Text = "Schema Data" };
-            data_button.Command = this._vm.HBDataBtnClick;
-            layout.AddSeparateRow(data_button, null);
+            layout.AddRow("People:", pplByProgram);
+            layout.AddRow(null, pplDP);
 
 
-            this.Content = layout;
+            //Get ElecEquipment
+            var eqpDP = new Button();
+            eqpDP.Bind((t) => t.Enabled, vm, v => v.ElecEquipment.IsBtnEnabled);
+            eqpDP.TextBinding.Bind(vm, _ => _.ElecEquipment.BtnName);
+            eqpDP.Command = vm.RoomHVACCommand;
+            var eqpByProgram = new CheckBox() { Text = vm.ByProgramType };
+            eqpByProgram.CheckedBinding.Bind(vm, _ => _.ElecEquipment.IsCheckboxChecked);
+
+            layout.AddRow("ElecEquipment:", eqpByProgram);
+            layout.AddRow(null, eqpDP);
+
+
+            //Get gas Equipment
+            var gasDP = new Button();
+            gasDP.Bind((t) => t.Enabled, vm, v => v.Gas.IsBtnEnabled);
+            gasDP.TextBinding.Bind(vm, _ => _.Gas.BtnName);
+            gasDP.Command = vm.RoomHVACCommand;
+            var gasByProgram = new CheckBox() { Text = vm.ByProgramType };
+            gasByProgram.CheckedBinding.Bind(vm, _ => _.Gas.IsCheckboxChecked);
+
+            layout.AddRow("GasEquipment:", gasByProgram);
+            layout.AddRow(null, gasDP);
+
+
+            //Get Ventilation
+            var ventDP = new Button();
+            ventDP.Bind((t) => t.Enabled, vm, v => v.Ventilation.IsBtnEnabled);
+            ventDP.TextBinding.Bind(vm, _ => _.Ventilation.BtnName);
+            ventDP.Command = vm.RoomHVACCommand;
+            var ventByProgram = new CheckBox() { Text = vm.ByProgramType };
+            ventByProgram.CheckedBinding.Bind(vm, _ => _.Ventilation.IsCheckboxChecked);
+
+            layout.AddRow("Ventilation:", ventByProgram);
+            layout.AddRow(null, ventDP);
+
+
+            //Get Infiltration
+            var infDP = new Button();
+            infDP.Bind((t) => t.Enabled, vm, v => v.Infiltration.IsBtnEnabled);
+            infDP.TextBinding.Bind(vm, _ => _.Infiltration.BtnName);
+            infDP.Command = vm.RoomHVACCommand;
+            var infByProgram = new CheckBox() { Text = vm.ByProgramType };
+            infByProgram.CheckedBinding.Bind(vm, _ => _.Infiltration.IsCheckboxChecked);
+
+            layout.AddRow("Infiltration:", infByProgram);
+            layout.AddRow(null, infDP);
+
+
+            //Get Setpoint
+            var stpDP = new Button();
+            stpDP.Bind((t) => t.Enabled, vm, v => v.Setpoint.IsBtnEnabled);
+            stpDP.TextBinding.Bind(vm, _ => _.Setpoint.BtnName);
+            stpDP.Command = vm.RoomHVACCommand;
+            var stpByProgram = new CheckBox() { Text = vm.ByProgramType };
+            stpByProgram.CheckedBinding.Bind(vm, _ => _.Setpoint.IsCheckboxChecked);
+
+            layout.AddRow("Setpoint:", stpByProgram);
+            layout.AddRow(null, stpDP);
+
+
+            //Get ServiceHotWater
+            var shwDP = new Button();
+            shwDP.Bind((t) => t.Enabled, vm, v => v.ServiceHotWater.IsBtnEnabled);
+            shwDP.TextBinding.Bind(vm, _ => _.ServiceHotWater.BtnName);
+            shwDP.Command = vm.RoomHVACCommand;
+            var shwByProgram = new CheckBox() { Text = vm.ByProgramType };
+            shwByProgram.CheckedBinding.Bind(vm, _ => _.ServiceHotWater.IsCheckboxChecked);
+
+            layout.AddRow("ServiceHotWater:", shwByProgram);
+            layout.AddRow(null, shwDP);
+
+            layout.AddRow(null);
+
+            layout.EndScrollable();
+            return layout;
         }
     }
 
---
+++ src/Honeybee.UI/ViewModel/CheckboxButtonViewModel.cs
@@ -0,0 +1,87 @@
+﻿using Eto.Forms;
+using System;
+
+namespace Honeybee.UI
+{
+    public class CheckboxButtonViewModel : ViewModelBase
+    {
+        public string Varies => "<varies>";
+        public bool IsVaries;
+        private HoneybeeSchema.IIDdBase _refObjProperty;
+        private HoneybeeSchema.IIDdBase refObjProperty
+        {
+            get => _refObjProperty;
+            set
+            {
+                _refObjProperty = value;
+                SetHBProperty(value);
+                BtnName = value?.DisplayName ?? value?.Identifier;
+            }
+        }
+        public Action<HoneybeeSchema.IIDdBase> SetHBProperty { get; private set; }
+
+
+        private string _btnName = "Override";
+        public string BtnName
+        {
+            get => IsVaries ? this.Varies : _btnName;
+            private set
+            {
+                IsCheckboxChecked = refObjProperty == null;
+                IsBtnEnabled = !IsCheckboxChecked;
+
+                IsVaries = value == this.Varies;
+                if (IsVaries)
+                    IsCheckboxChecked = false;
+
+                if (IsBtnEnabled)
+                    this.Set(() => _btnName = value, nameof(BtnName));
+            }
+        }
+
+        private bool _isBtnEnabled;
+
+        public bool IsBtnEnabled
+        {
+            get => _isBtnEnabled;
+            private set { this.Set(() => _isBtnEnabled = value, nameof(IsBtnEnabled)); }
+        }
+
+        private bool _isCheckboxChecked;
+
+        public bool IsCheckboxChecked
+        {
+            get => _isCheckboxChecked;
+
+            private set
+            {
+                this.Set(() => _isCheckboxChecked = value, nameof(IsCheckboxChecked));
+                IsBtnEnabled = !value;
+                if (_isCheckboxChecked)
+                    SetHBProperty(null);
+                else
+                {
+                    SetHBProperty(_refObjProperty);
+                }
+            }
+        }
+
+
+        public CheckboxButtonViewModel(Action<HoneybeeSchema.IIDdBase> setAction)
+        {
+            this.SetHBProperty = setAction;
+        }
+
+        public void SetBtnName(string name)
+        {
+            this.BtnName = name;
+        }
+
+        public void SetPropetyObj(HoneybeeSchema.IIDdBase obj)
+        {
+            this.refObjProperty = obj;
+        }
+    }
+
+
+}
