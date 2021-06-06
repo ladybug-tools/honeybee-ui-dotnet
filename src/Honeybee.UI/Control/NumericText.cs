7a7faa4156154eceeb143372fd14ac9a4aeb96a0
MingboPeng <mingo1214@gmail.com>
2021-06-05 20:20:16 -0400

    fix(control): add editedColor for user's inputs

--- src/Honeybee.UI/Control/NumericText.cs
+++ src/Honeybee.UI/Control/NumericText.cs
@@ -25,6 +25,15 @@ namespace Honeybee.UI
         public override bool IsTextValid(string text) => double.TryParse(this.Text, out var value);
     }
 
+    public class StringText : ValidableText
+    {
+        public StringText()
+        {
+        }
+
+        public override bool IsTextValid(string text) => !string.IsNullOrEmpty(this.Text);
+    }
+
 
     public abstract class ValidableText : TextBox
     {
@@ -40,13 +49,16 @@ namespace Honeybee.UI
         {
             this._defaultText = value?.ToString();
         }
-
-        protected override void OnShown(EventArgs e)
+        protected override void OnLoadComplete(EventArgs e)
         {
-            base.OnShown(e);
+            base.OnLoadComplete(e);
+            this.TextColor = GetTextColor();
             this._defaultBackground = this.BackgroundColor;
-            this.TextColor = _defaultText == this.Text ? _gry : _defaultTextColor;
         }
+        //protected override void OnShown(EventArgs e)
+        //{
+        //    base.OnShown(e);
+        //}
         protected override void OnTextChanged(EventArgs e)
         {
             base.OnTextChanged(e);
@@ -57,7 +69,7 @@ namespace Honeybee.UI
             if (!this.Loaded)
                 return;
 
-            this.TextColor = _defaultText == this.Text ? _gry : _defaultTextColor;
+            this.TextColor = GetTextColor();
             this.BackgroundColor = IsTextValid(this.Text) ? _defaultBackground : _red;
 
         }
@@ -65,13 +77,21 @@ namespace Honeybee.UI
         private Eto.Drawing.Color _defaultBackground;
         private Eto.Drawing.Color _defaultTextColor;
         private Eto.Drawing.Color _blk = Eto.Drawing.Color.FromArgb(0, 0, 0);
+        private Eto.Drawing.Color _disabledGry = Eto.Drawing.Color.FromArgb(200, 200, 200);
         private Eto.Drawing.Color _gry = Eto.Drawing.Color.FromArgb(150, 150, 150);
         private Eto.Drawing.Color _red = Eto.Drawing.Color.FromArgb(255, 100, 100);
+        private Eto.Drawing.Color _editedColor = Eto.Drawing.Color.FromArgb(150, 27, 100);
 
         protected override void OnEnabledChanged(EventArgs e)
         {
             base.OnEnabledChanged(e);
-            this.TextColor = this.Enabled ? _defaultTextColor : _gry;
+            this.TextColor = GetTextColor();
+        }
+
+        private Eto.Drawing.Color GetTextColor()
+        {
+            var defalutColor = _defaultText == this.Text ?  _defaultTextColor: _editedColor;
+            return this.Enabled ? defalutColor : _disabledGry;
         }
     }
 
