b13955f38d70f8c3d884fb8c65a3dac0066e3461
MingboPeng <mingo1214@gmail.com>
2021-08-04 23:55:52 -0400

    fix(ConstructionSetManager): update ConstructionSetManager

--- src/Honeybee.UI/Dialog/Dialog_ConstructionSetManager.cs
+++ src/Honeybee.UI/Dialog/Dialog_ConstructionSetManager.cs
@@ -10,9 +10,8 @@ namespace Honeybee.UI
 {
     public class Dialog_ConstructionSetManager : Dialog<List<HB.Energy.IBuildingConstructionset>>
     {
-        private GridView _gd;
         private bool _returnSelectedOnly;
-        private ModelEnergyProperties _modelEnergyProperties { get; set; }
+        private ConstructionSetManagerViewModel _vm { get; set; }
 
         private Dialog_ConstructionSetManager()
         {
@@ -20,47 +19,53 @@ namespace Honeybee.UI
             Resizable = true;
             Title = $"ConstructionSet Manager - {DialogHelper.PluginName}";
             WindowStyle = WindowStyle.Default;
-            MinimumSize = new Size(650, 300);
+            MinimumSize = new Size(750, 300);
             this.Icon = DialogHelper.HoneybeeIcon;
         }
 
         public Dialog_ConstructionSetManager(ref ModelEnergyProperties libSource, bool returnSelectedOnly = false) : this()
         {
             this._returnSelectedOnly = returnSelectedOnly;
-            this._modelEnergyProperties = libSource;
-
-            var constrSets = libSource.ConstructionSetList;
-            Content = Init(constrSets);
+            this._vm = new ConstructionSetManagerViewModel(libSource, this);
+            Content = Init();
         }
 
 
-        private DynamicLayout Init(IEnumerable<HB.Energy.IBuildingConstructionset> constrSets)
+        private DynamicLayout Init()
         {
             var layout = new DynamicLayout();
-            layout.DefaultPadding = new Padding(10);
+            layout.DefaultPadding = new Padding(5);
             layout.DefaultSpacing = new Size(5, 5);
 
             var addNew = new Button { Text = "Add" };
-            addNew.Command = AddCommand;
+            addNew.Command = _vm.AddCommand;
 
             var duplicate = new Button { Text = "Duplicate" };
-            duplicate.Command = DuplicateCommand;
+            duplicate.Command = _vm.DuplicateCommand;
 
             var edit = new Button { Text = "Edit" };
-            edit.Command = EditCommand;
+            edit.Command = _vm.EditCommand;
 
             var remove = new Button { Text = "Remove" };
-            remove.Command = RemoveCommand;
+            remove.Command = _vm.RemoveCommand;
 
             layout.AddSeparateRow("Construction Sets:", null, addNew, duplicate, edit, remove);
 
-            var gd = GenGridView(constrSets);
-            _gd = gd;
+            // search bar
+            var filter = new TextBox() { PlaceholderText = "Filter" };
+            filter.TextBinding.Bind(_vm, _ => _.FilterKey);
+            layout.AddRow(filter);
+
+            var gd = GenGridView();
             gd.Height = 250;
             layout.AddRow(gd);
 
+            // counts
+            var counts = new Label();
+            counts.TextBinding.Bind(_vm, _ => _.Counts);
+            layout.AddSeparateRow(counts, null);
 
-            gd.CellDoubleClick += (s, e) => EditCommand.Execute(null);
+            gd.CellDoubleClick += (s, e) => _vm.EditCommand.Execute(null);
 
             DefaultButton = new Button { Text = "OK" };
             DefaultButton.Click += (sender, e) => OkCommand.Execute(null);
@@ -68,151 +73,143 @@ namespace Honeybee.UI
             AbortButton = new Button { Text = "Cancel" };
             AbortButton.Click += (sender, e) => Close();
             layout.AddSeparateRow(null, DefaultButton, AbortButton, null);
+            layout.AddRow(null);
             return layout;
 
         }
 
-        private GridView GenGridView(IEnumerable<object> items)
+        private GridView GenGridView()
         {
-            items = items ?? new List<HB.Energy.IBuildingConstructionset>();
-            var gd = new GridView() { DataStore = items };
-  
-            var nameTB = new TextBoxCell
-            {
-                Binding = Binding.Delegate<HB.ConstructionSetAbridged, string>(r => r.DisplayName ?? r.Identifier)
+            var gd = new GridView();
+            gd.Bind(_ => _.DataStore, _vm, _ => _.GridViewDataCollection);
+            gd.SelectedItemsChanged += (s, e) => {
+                _vm.SelectedData = gd.SelectedItem as ConstructionSetViewData;
             };
-            gd.Columns.Add(new GridColumn { DataCell = nameTB, HeaderText = "Name" });
 
+            gd.Height = 250;
+            gd.Columns.Add(new GridColumn
+            {
+                DataCell = new TextBoxCell { Binding = Binding.Delegate<ConstructionSetViewData, string>(r => r.Name) },
+                HeaderText = "Name",
+                Sortable = true
+            });
+            gd.Columns.Add(new GridColumn
+            {
+                DataCell = new CheckBoxCell { Binding = Binding.Delegate<ConstructionSetViewData, bool?>(r => r.HasWallSet) },
+                HeaderText = "Wall",
+                Sortable = true
+            });
+            gd.Columns.Add(new GridColumn
+            {
+                DataCell = new CheckBoxCell { Binding = Binding.Delegate<ConstructionSetViewData, bool?>(r => r.HasRoofCeilingSet) },
+                HeaderText = "RoofCeiling",
+                Sortable = true
+            });
+            gd.Columns.Add(new GridColumn
+            {
+                DataCell = new CheckBoxCell { Binding = Binding.Delegate<ConstructionSetViewData, bool?>(r => r.HasFloorSet) },
+                HeaderText = "Floor",
+                Sortable = true
+            });
+            gd.Columns.Add(new GridColumn
+            {
+                DataCell = new CheckBoxCell { Binding = Binding.Delegate<ConstructionSetViewData, bool?>(r => r.HasApertureSet) },
+                HeaderText = "Aperture",
+                Sortable = true
+            });
+            gd.Columns.Add(new GridColumn
+            {
+                DataCell = new CheckBoxCell { Binding = Binding.Delegate<ConstructionSetViewData, bool?>(r => r.HasDoorSet) },
+                HeaderText = "Door",
+                Sortable = true
+            });
+            gd.Columns.Add(new GridColumn
+            {
+                DataCell = new CheckBoxCell { Binding = Binding.Delegate<ConstructionSetViewData, bool?>(r => r.HasAirBoundaryConstruction) },
+                HeaderText = "AirBoundary",
+                Sortable = true
+            });
+            gd.Columns.Add(new GridColumn
+            {
+                DataCell = new CheckBoxCell { Binding = Binding.Delegate<ConstructionSetViewData, bool?>(r => r.HasShadeSet) },
+                HeaderText = "Shade",
+                Sortable = true
+            });
+            gd.Columns.Add(new GridColumn
+            {
+                DataCell = new CheckBoxCell { Binding = Binding.Delegate<ConstructionSetViewData, bool?>(r => r.Locked) },
+                HeaderText = "Locked",
+                Sortable = true
+            });
+            gd.Columns.Add(new GridColumn
+            {
+                DataCell = new TextBoxCell { Binding = Binding.Delegate<ConstructionSetViewData, string>(r => r.Source) },
+                HeaderText = "Source",
+                Sortable = true
+            });
+            // sorting by header
+            gd.ColumnHeaderClick += OnColumnHeaderClick;
             return gd;
         }
 
 
-
-        public RelayCommand AddCommand => new RelayCommand(() =>
+        private string _currentSortByColumn;
+        private void OnColumnHeaderClick(object sender, GridColumnEventArgs e)
         {
-            var gd = this._gd;
-            var dialog = new Honeybee.UI.Dialog_OpsConstructionSet(this._modelEnergyProperties);
-            var dialog_rc = dialog.ShowModal(this);
-
-            var cSet = dialog_rc.constructionSet;
-            var contrs = dialog_rc.constructions;
-            var mats = dialog_rc.materials;
-            if (cSet != null)
+            var cell = e.Column.DataCell;
+            var colName = e.Column.HeaderText;
+            System.Func<ConstructionSetViewData, string> sortFunc = null;
+            var isNumber = false;
+            switch (colName)
             {
 
-                var existingConstructionIds = this._modelEnergyProperties.Constructions.Select(_ => (_.Obj as HB.IDdEnergyBaseModel).Identifier);
-                var existingMaterialIds = this._modelEnergyProperties.Materials.Select(_ => (_.Obj as HB.IDdEnergyBaseModel).Identifier);
-
-                // add constructions
-                var newConstrs = contrs.Where(_ => !existingConstructionIds.Any(c => c == _.Identifier)).ToList();
-                this._modelEnergyProperties.AddConstructions(newConstrs);
-
-                // add materials
-                var newMats = mats.Where(_ => !existingMaterialIds.Any(m => m == _.Identifier)).ToList();
-                this._modelEnergyProperties.AddMaterials(newMats);
-
-                // add program type
-                var d = gd.DataStore.Select(_ => _ as ConstructionSetAbridged).ToList();
-                d.Add(cSet);
-                gd.DataStore = d;
-
-            }
-        });
-
-        public RelayCommand DuplicateCommand => new RelayCommand(() =>
-        {
-            var gd = this._gd;
-            if (gd.SelectedItem == null)
-            {
-                MessageBox.Show(this, "Nothing is selected to duplicate!");
-                return;
-            }
-
-            var id = Guid.NewGuid().ToString();
-
-            var dup = (gd.SelectedItem as HB.ConstructionSetAbridged).DuplicateConstructionSetAbridged();
-
-            dup.Identifier = id;
-            dup.DisplayName = string.IsNullOrEmpty(dup.DisplayName) ? $"New Duplicate {id.Substring(0, 5)}" : $"{dup.DisplayName}_dup";
-            var dialog = new Honeybee.UI.Dialog_ConstructionSet(this._modelEnergyProperties, dup);
-            var dialog_rc = dialog.ShowModal(this);
-            if (dialog_rc != null)
-            {
-                var d = gd.DataStore.OfType<ConstructionSetAbridged>().ToList();
-                d.Add(dialog_rc);
-                gd.DataStore = d;
-
-            }
-        });
-
-        public RelayCommand EditCommand => new RelayCommand(() =>
-        {
-            var gd = this._gd;
-            var selected = gd.SelectedItem as HB.ConstructionSetAbridged;
-            if (selected == null)
-            {
-                MessageBox.Show(this, "Nothing is selected to edit!");
-                return;
+                case "Name":
+                    sortFunc = (ConstructionSetViewData _) => _.Name;
+                    break;
+                case "Wall":
+                    sortFunc = (ConstructionSetViewData _) => _.HasWallSet.ToString();
+                    break;
+                case "RoofCeiling":
+                    sortFunc = (ConstructionSetViewData _) => _.HasRoofCeilingSet.ToString();
+                    break;
+                case "Floor":
+                    sortFunc = (ConstructionSetViewData _) => _.HasFloorSet.ToString();
+                    break;
+                case "Aperture":
+                    sortFunc = (ConstructionSetViewData _) => _.HasApertureSet.ToString();
+                    break;
+                case "Door":
+                    sortFunc = (ConstructionSetViewData _) => _.HasDoorSet.ToString();
+                    break;
+                case "AirBoundary":
+                    sortFunc = (ConstructionSetViewData _) => _.HasAirBoundaryConstruction.ToString();
+                    break;
+                case "Shade":
+                    sortFunc = (ConstructionSetViewData _) => _.HasShadeSet.ToString();
+                    break;
+                case "Locked":
+                    sortFunc = (ConstructionSetViewData _) => _.Locked.ToString();
+                    break;
+                case "Source":
+                    sortFunc = (ConstructionSetViewData _) => _.Source;
+                    break;
+                default:
+                    break;
             }
 
-            var dup = (gd.SelectedItem as HB.ConstructionSetAbridged).DuplicateConstructionSetAbridged();
+            if (sortFunc == null) return;
 
-            var dialog = new Honeybee.UI.Dialog_ConstructionSet(this._modelEnergyProperties, dup);
-            var dialog_rc = dialog.ShowModal(this);
-            if (dialog_rc != null)
-            {
-                var index = gd.SelectedRow;
-                var newDataStore = gd.DataStore.OfType<ConstructionSetAbridged>().ToList();
-                newDataStore.RemoveAt(index);
-                newDataStore.Insert(index, dialog_rc);
-                gd.DataStore = newDataStore;
+            var descend = colName == _currentSortByColumn;
+            _vm.SortList(sortFunc, isNumber, descend);
 
-            }
-        });
+            _currentSortByColumn = colName == _currentSortByColumn ? string.Empty : colName;
 
-        public RelayCommand RemoveCommand => new RelayCommand(() =>
-        {
-            var gd = this._gd;
-            var selected = gd.SelectedItem as HB.Energy.IBuildingConstructionset;
-            if (selected == null)
-            {
-                MessageBox.Show(this, "Nothing is selected to edit!");
-                return;
-            }
+        }
 
-            if (selected.Identifier.Equals("Default Generic Construction Set"))
-            {
-                MessageBox.Show(this, $"{selected.DisplayName ?? selected.Identifier } cannot be removed, because it is set to default global construction set.");
-                return;
-            }
-
-            var res = MessageBox.Show(this, $"Are you sure you want to delete:\n {selected.DisplayName ?? selected.Identifier }", MessageBoxButtons.YesNo);
-            if (res == DialogResult.Yes)
-            {
-                var newDataStore = gd.DataStore.Where(_ => _ != selected).ToList();
-                gd.DataStore = newDataStore;
-            }
-        });
 
         public RelayCommand OkCommand => new RelayCommand(() =>
         {
-            var gd = this._gd;
-            var allItems = gd.DataStore.Select(_ => _ as HB.Energy.IBuildingConstructionset).ToList();
-            var itemsToReturn = allItems;
-
-            if (this._returnSelectedOnly)
-            {
-                var d = gd.SelectedItem as HB.Energy.IBuildingConstructionset;
-                if (d == null)
-                {
-                    MessageBox.Show(this, "Nothing is selected!");
-                    return;
-                }
-                itemsToReturn = new List<HB.Energy.IBuildingConstructionset>() { d };
-            }
-
-            this._modelEnergyProperties.ConstructionSets.Clear();
-            this._modelEnergyProperties.AddConstructionSets(allItems);
+            var itemsToReturn = _vm.GetUserItems(_returnSelectedOnly);
             Close(itemsToReturn);
         });
 
