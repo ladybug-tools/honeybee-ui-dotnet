e6a332b44d9aae5fff04593b7fdada3cdedcd056
MingboPeng <mingo1214@gmail.com>
2021-11-15 16:08:10 +0800

    fix(AirBoundary): fix AirBoundary construction's schedule input

--- src/Honeybee.UI/Dialog/Dialog_Construction_AirBoundary.cs
+++ src/Honeybee.UI/Dialog/Dialog_Construction_AirBoundary.cs
@@ -7,9 +7,14 @@ namespace Honeybee.UI
 
     public class Dialog_Construction_AirBoundary : Dialog_ResourceEditor<HB.AirBoundaryConstructionAbridged>
     {
-        public Dialog_Construction_AirBoundary(HB.AirBoundaryConstructionAbridged airBoundaryConst, bool lockedMode = false)
+        private HB.ModelEnergyProperties _lib;
+        private Button _airMixSch;
+        private HB.AirBoundaryConstructionAbridged _hbObj;
+
+        public Dialog_Construction_AirBoundary(HB.ModelEnergyProperties libSource, HB.AirBoundaryConstructionAbridged airBoundaryConst, bool lockedMode = false)
         {
-            var _hbObj = airBoundaryConst;
+            _lib = libSource;
+            _hbObj = airBoundaryConst;
 
             Padding = new Padding(10);
             Resizable = true;
@@ -47,8 +52,10 @@ namespace Honeybee.UI
             layout.AddRow(airMx);
 
             //AirMixingSchedule
-            var airMxSch = new TextBox() { Enabled = false};
-            airMxSch.TextBinding.Bind(() => _hbObj.AirMixingSchedule, (v) => _hbObj.AirMixingSchedule = v);
+            var airMxSch = new Button();
+            _airMixSch = airMxSch;
+            airMxSch.Text = _hbObj.AirMixingSchedule;
+            airMxSch.Click += AirMxSch_Click;
             layout.AddRow(nameof(_hbObj.AirMixingSchedule));
             layout.AddRow(airMxSch);
 
@@ -68,5 +75,16 @@ namespace Honeybee.UI
 
         }
 
+        private void AirMxSch_Click(object sender, System.EventArgs e)
+        {
+            var sch_dialog = new Honeybee.UI.Dialog_ScheduleRulesetManager(ref _lib, true);
+            var dialog_rc = sch_dialog.ShowModal(this);
+            if (dialog_rc != null)
+            {
+                var sch = dialog_rc[0];
+                _airMixSch.Text = sch.DisplayName ?? sch.Identifier;
+                _hbObj.AirMixingSchedule = sch.Identifier;
+            }
+        }
     }
 }
