e836afc06bec03a4ab7692c2bec2f5b665b367f0
MingboPeng <mingo1214@gmail.com>
2021-11-22 20:42:32 +0800

    feat(UserData):  add UserData to RoomProperty

---
+++ src/Honeybee.UI/Dialog/Dialog_AddUserData.cs
@@ -0,0 +1,77 @@
+﻿using Eto.Drawing;
+using Eto.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Honeybee.UI
+{
+    public class Dialog_AddUserData : Dialog<UserDataItem>
+    {
+        public Dialog_AddUserData(IEnumerable<UserDataItem> allItems, UserDataItem userData = default)
+        {
+            try
+            {
+                Title = $"User Data - {DialogHelper.PluginName}";
+                WindowStyle = WindowStyle.Default;
+                this.Icon = DialogHelper.HoneybeeIcon;
+
+                var p = new DynamicLayout();
+                p.Width = 400;
+                p.DefaultSpacing = new Size(4, 4);
+                p.DefaultPadding = new Padding(4);
+
+
+                var k = new TextBox();
+                k.Text = userData?.Key;
+                p.AddRow("Key");
+                p.AddRow(k);
+
+                var v = new TextArea() {Height = 100 };
+                v.Text = userData?.Value;
+                p.AddRow("Value");
+                p.AddRow(v);
+
+                p.AddRow(null);
+
+                var OKButton = new Button() { Text = "OK" };
+                OKButton.Click += (s, e) =>
+                {
+                    try
+                    {
+                        var key = k.Text.Trim();
+                        if (allItems != null)
+                        {
+                            var keys = allItems.Select(_ => _.Key);
+                            if (keys.Contains(key) && userData?.Key != key)
+                            {
+                                MessageBox.Show($"Key:[{key}] already exists, please use a different key");
+                                return;
+                            }
+                        }
+
+                        this.Close(new UserDataItem(key, v.Text));
+                    }
+                    catch (Exception er)
+                    {
+                        MessageBox.Show(er.Message);
+                    }
+
+                };
+
+                AbortButton = new Button { Text = "Cancel" };
+                AbortButton.Click += (sender, e) => Close();
+
+                p.AddSeparateRow(null, OKButton, this.AbortButton, null);
+                p.Add(null);
+                this.Content = p;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                //throw;
+            }
+
+        }
+    }
+}
---
+++ src/Honeybee.UI/ViewModel/UserDataViewModel.cs
@@ -0,0 +1,157 @@
+﻿using Eto.Forms;
+using System;
+using HoneybeeSchema;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Honeybee.UI
+{
+    public class UserDataItem
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public UserDataItem(string key, string value)
+        {
+            this.Key = key;
+            this.Value = value;
+        }
+    }
+
+
+    public class UserDataViewModel : ViewModelBase
+    {
+        private List<UserDataItem> refObjProperty = new List<UserDataItem>();
+
+        private bool _isPanelEnabled = true;
+
+        public bool IsPanelEnabled
+        {
+            get => _isPanelEnabled;
+            private set { this.Set(() => _isPanelEnabled = value, nameof(IsPanelEnabled)); }
+        }
+
+        private bool _isCheckboxChecked;
+
+        public bool IsCheckboxChecked
+        {
+            get => _isCheckboxChecked;
+
+            protected set
+            {
+                this.Set(() => _isCheckboxChecked = value, nameof(IsCheckboxChecked));
+                IsPanelEnabled = !value;
+                if (_isCheckboxChecked)
+                    SetHBProperty(null);
+                else
+                {
+                    SetHBProperty(UserItemsToDic(refObjProperty));
+                }
+            }
+        }
+
+        private DataStoreCollection<UserDataItem> _gridViewDataCollection = new DataStoreCollection<UserDataItem>();
+        public DataStoreCollection<UserDataItem> GridViewDataCollection
+        {
+            get => _gridViewDataCollection;
+            set => this.Set(() => _gridViewDataCollection = value, nameof(_gridViewDataCollection));
+        }
+
+        public UserDataItem _selectedItem;
+        public UserDataItem SelectedItem
+        {
+            get => _selectedItem;
+            set => this.Set(() => _selectedItem = value, nameof(SelectedItem));
+        }
+        public Action<Dictionary<string, string>> SetHBProperty { get; private set; }
+
+        private Control control;
+
+        public UserDataViewModel( List<object> allUserData, Action<Dictionary<string, string>> setAction, Control control)
+        {
+            this.control = control;
+            this.SetHBProperty = setAction;
+
+            this.refObjProperty = allUserData.FirstOrDefault() as List<UserDataItem> ?? new List<UserDataItem>();
+            
+
+            if (allUserData.Distinct().Count() == 1 && allUserData.FirstOrDefault() == null)
+            {
+                this.IsCheckboxChecked = true;
+            }
+
+            this.GridViewDataCollection = new DataStoreCollection<UserDataItem>(this.refObjProperty);
+        }
+
+        public object MatchObj()
+        {
+            // by room program type
+            if (this.IsCheckboxChecked)
+                return null;
+
+            var dic = UserItemsToDic(this.refObjProperty);
+            var s = Newtonsoft.Json.JsonConvert.SerializeObject(dic);
+            return s;
+        }
+
+        private void UpdateRefObj()
+        {
+            this.refObjProperty = this.GridViewDataCollection.Select(_ => _).ToList();
+            var dic = UserItemsToDic(this.refObjProperty);
+            SetHBProperty(dic);
+        }
+
+        private Dictionary<string,string> UserItemsToDic(List<UserDataItem> userItems)
+        {
+            var dic = userItems.ToDictionary(_ => _.Key, _ => _.Value);
+            return dic;
+        }
+
+        public System.Windows.Input.ICommand AddDataCommand => new RelayCommand(() =>
+        {
+            var dialog = new Dialog_AddUserData(this.GridViewDataCollection, null);
+            var rc = dialog.ShowModal(this.control);
+            if (rc != null)
+            {
+                this.GridViewDataCollection.Add(rc);
+                UpdateRefObj();
+                //gd.DataStore = this.GridViewDataCollection;
+            }
+
+        });
+        public System.Windows.Input.ICommand EditDataCommand => new RelayCommand(() =>
+        {
+            var sel = this.SelectedItem;
+            if (sel == null)
+            {
+                MessageBox.Show($"Nothing is selected!");
+                return;
+            }
+
+            var dialog = new Dialog_AddUserData(this.GridViewDataCollection, sel);
+            var rc = dialog.ShowModal(this.control);
+            if (rc != null)
+            {
+                var i = this.refObjProperty.FindIndex(_=>_.Key == sel.Key);
+                this.GridViewDataCollection.RemoveAt(i);
+                this.GridViewDataCollection.Insert(i, rc);
+                UpdateRefObj();
+            }
+
+        });
+
+        public System.Windows.Input.ICommand RemoveDataCommand => new RelayCommand(() =>
+        {
+            var sel = this.SelectedItem;
+            if (sel == null)
+            {
+                MessageBox.Show($"Nothing is selected!");
+                return;
+            }
+
+            this.GridViewDataCollection.Remove(sel);
+            UpdateRefObj();
+        });
+    }
+
+
+}
