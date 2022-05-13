a820a5eb58efb03d2d60505dd859c83ad832a3a0
MingboPeng <mingo1214@gmail.com>
2022-05-13 18:21:47 +0800

    feat(ValidationReport):

---
+++ src/Honeybee.UI/Dialog/Dialog_Error.cs
@@ -0,0 +1,77 @@
+﻿using Eto.Forms;
+using System;
+
+namespace Honeybee.UI
+{
+    public class Dialog_Error : Eto.Forms.Dialog
+    {
+        private ErrorViewModel _vm;
+
+        public Dialog_Error(HoneybeeSchema.ValidationReport report, Action<HoneybeeSchema.ValidationError> showAction = default)
+        {
+            this.Title = $"Error - {DialogHelper.PluginName}";
+            this.Width = 600;
+            this.Icon = DialogHelper.HoneybeeIcon;
+
+            _vm = new ErrorViewModel(report, this);
+
+            var message = new TextArea() { Height = 80 };
+            var nextBtn = new Button() { Text = ">>"};
+            var preBtn = new Button() { Text = "<<" };
+            var showBtn = new Button() { Text = "Show" };
+            var moreInfoBtn = new Button() { Text = "More Info" };
+
+            var totalErrorCount = new Label();
+            var currentErrorIndex = new Label();
+
+            message.TextBinding.Bind(_vm, _=>_.CurrentErrorMessage);
+            totalErrorCount.TextBinding.Bind(_vm, _=>_.TotalErrorCount);
+            currentErrorIndex.TextBinding.Bind(_vm, _=>_.CurrentErrorIndex);
+            nextBtn.Bind(_ => _.Enabled, _vm, _ => _.NextBtnEnabled);
+            preBtn.Bind(_ => _.Enabled, _vm, _ => _.PreBtnEnabled);
+            nextBtn.Command = _vm.NextBtnCommand;
+            preBtn.Command = _vm.PreBtnCommand;
+            moreInfoBtn.Command = _vm.MoreInfoCommand;
+            showBtn.Command = _vm.ShowCommand;
+
+            preBtn.Enabled = false;
+            showBtn.Enabled = showAction != null;
+
+          
+            var group = new GroupBox() { Text = $"Found {_vm.TotalErrorCount} Error(s)" };
+            var groupLayout = new DynamicLayout();
+            groupLayout.DefaultSpacing = new Eto.Drawing.Size(5, 5);
+            groupLayout.DefaultPadding = new Eto.Drawing.Padding(5);
+            groupLayout.AddSeparateRow(message);
+            groupLayout.AddSeparateRow(preBtn, currentErrorIndex, nextBtn, null, showBtn, moreInfoBtn);
+            group.Content = groupLayout;
+
+            var OkBtn = new Eto.Forms.Button() { Text = "OK" };
+            OkBtn.Click += (s, e) => {
+                //if (_vm.Validate())
+                //{
+                //    var lg = _vm.GetLegend();
+                //    this.Close(lg);
+                //}
+               
+            };
+            this.AbortButton = new Eto.Forms.Button() { Text = "Close" };
+            this.AbortButton.Click += (s, e) => { this.Close(); };
+
+
+            var layout = new Eto.Forms.DynamicLayout();
+            layout.DefaultSpacing = new Eto.Drawing.Size(5, 2);
+            layout.DefaultPadding = new Eto.Drawing.Padding(4);
+            layout.AddSeparateRow(group);
+      
+            layout.AddSeparateRow(null, this.AbortButton);
+            layout.AddRow(null);
+            this.Content = layout;
+
+        }
+
+
+
+    }
+
+}
---
+++ src/Honeybee.UI/ViewModel/ErrorViewModel.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Honeybee.UI
+{
+    public class ErrorViewModel : ViewModelBase
+    {
+        private string totalErrorCount;
+        public string TotalErrorCount
+        {
+            get => totalErrorCount;
+            set => Set(() => totalErrorCount = value, nameof(TotalErrorCount));
+        }
+
+        //private string fatalError;
+        //public string FatalError
+        //{
+        //    get => fatalError;
+        //    set => Set(() => fatalError = value, nameof(FatalError));
+        //}
+
+        private int _currentIndex = 1;
+        private string currentErrorIndex = "XX of XX";
+        public string CurrentErrorIndex
+        {
+            get => currentErrorIndex;
+            set => Set(() => currentErrorIndex = value, nameof(CurrentErrorIndex));
+        }
+
+        private string currentErrorMessage;
+        public string CurrentErrorMessage
+        {
+            get => currentErrorMessage;
+            set => Set(() => currentErrorMessage = value, nameof(CurrentErrorMessage));
+        }
+
+        private bool _preBtnEnabled;
+        public bool PreBtnEnabled
+        {
+            get => _preBtnEnabled;
+            set => Set(() => _preBtnEnabled = value, nameof(PreBtnEnabled));
+        }
+
+        private bool _nextBtnEnabled;
+        public bool NextBtnEnabled
+        {
+            get => _nextBtnEnabled;
+            set => Set(() => _nextBtnEnabled = value, nameof(NextBtnEnabled));
+        }
+
+        private HoneybeeSchema.ValidationError _currentError;
+
+        public HoneybeeSchema.ValidationError CurrentError
+        {
+            get => _currentError;
+            set 
+            { 
+                _currentError = value;
+                CurrentErrorMessage = $"Error Code: {value?.Code} - {value?.ElementType}({value?.ExtensionType}){Environment.NewLine}{value?.Message}" ;
+            }
+        }
+
+        private List<HoneybeeSchema.ValidationError> _errors;
+
+        private HoneybeeSchema.ValidationReport _report;
+        private Eto.Forms.Control _control;
+        public ErrorViewModel(HoneybeeSchema.ValidationReport report, Eto.Forms.Control control)
+        {
+            _report = report;
+            _control = control;
+
+            _errors = new List<HoneybeeSchema.ValidationError>();
+            var errorCount = (report.Errors?.Count).GetValueOrDefault();
+          
+            //this.FatalError = report.FatalError;
+
+            if (!string.IsNullOrEmpty(report.FatalError))
+            {
+                var fatalError = new HoneybeeSchema.ValidationError("999999", HoneybeeSchema.ExtensionTypes.Core, HoneybeeSchema.ObjectTypes.Room, "FatalElementID", report.FatalError);
+                _errors.Add(fatalError);
+
+                errorCount += 1;
+
+            }
+
+            if (report.Errors != null && report.Errors.Any())
+            {
+                _errors.AddRange(report.Errors);
+            }
+
+            CurrentError = _errors.First();
+
+            this.TotalErrorCount = errorCount.ToString();
+            this.CurrentErrorIndex = $"1 of {TotalErrorCount}";
+
+            this.PreBtnEnabled = false;
+            this.NextBtnEnabled = _errors.Count > 1;
+
+        }
+
+        public Eto.Forms.RelayCommand PreBtnCommand => new Eto.Forms.RelayCommand(() =>
+        {
+            if (_currentIndex <= 1)
+                return;
+
+            _currentIndex -= 1;
+            this.PreBtnEnabled = _currentIndex > 1;
+            this.NextBtnEnabled = _currentIndex < _errors.Count;
+            this.CurrentErrorIndex = $"{_currentIndex} of {this.TotalErrorCount}";
+
+            this.CurrentError = this._errors.ElementAt(_currentIndex - 1);
+        });
+
+        public Eto.Forms.RelayCommand NextBtnCommand => new Eto.Forms.RelayCommand(() =>
+        {
+            if (_currentIndex >= _errors.Count)
+                return;
+
+            _currentIndex += 1;
+            this.PreBtnEnabled = _currentIndex > 1;
+            this.NextBtnEnabled = _currentIndex < _errors.Count;
+            this.CurrentErrorIndex = $"{_currentIndex} of {this.TotalErrorCount}";
+
+            this.CurrentError = this._errors.ElementAt(_currentIndex - 1);
+        });
+
+        public Eto.Forms.RelayCommand MoreInfoCommand => new Eto.Forms.RelayCommand(() =>
+        {
+            if (this.CurrentError == null)
+                return;
+
+            Dialog_Message.Show(this._control, this.CurrentError.ToJson(true));
+        });
+
+        public Eto.Forms.RelayCommand<Action<HoneybeeSchema.ValidationError>> ShowCommand => new Eto.Forms.RelayCommand<Action<HoneybeeSchema.ValidationError>>((Action<HoneybeeSchema.ValidationError> action) =>
+        {
+            if (this.CurrentError == null)
+                return;
+
+            var err = this.CurrentError;
+            if (IsGeometryType(err.ElementType))
+            {
+                action?.Invoke(err);
+            }
+            else
+            {
+                Eto.Forms.MessageBox.Show($"{err.ElementType} is not a geometry element type, which cannot be shown!");
+            }
+
+        });
+
+
+        private static bool IsGeometryType(HoneybeeSchema.ObjectTypes type)
+        {
+            switch (type)
+            {
+                case HoneybeeSchema.ObjectTypes.Shade:
+                case HoneybeeSchema.ObjectTypes.Aperture:
+                case HoneybeeSchema.ObjectTypes.Door:
+                case HoneybeeSchema.ObjectTypes.Face:
+                case HoneybeeSchema.ObjectTypes.Room:
+                case HoneybeeSchema.ObjectTypes.SensorGrid:
+                case HoneybeeSchema.ObjectTypes.View:
+                    return true;
+                case HoneybeeSchema.ObjectTypes.Modifier:
+                case HoneybeeSchema.ObjectTypes.ModifierSet:
+                case HoneybeeSchema.ObjectTypes.Material:
+                case HoneybeeSchema.ObjectTypes.Construction:
+                case HoneybeeSchema.ObjectTypes.ConstructionSet:
+                case HoneybeeSchema.ObjectTypes.ScheduleTypeLimit:
+                case HoneybeeSchema.ObjectTypes.Schedule:
+                case HoneybeeSchema.ObjectTypes.ProgramType:
+                case HoneybeeSchema.ObjectTypes.HVAC:
+                case HoneybeeSchema.ObjectTypes.SHW:
+                    return false;
+                default:
+                    throw new System.ArgumentException($"{type} is not supported, please contact developers!");
+            }
+        }
+    }
+
+    
+}
