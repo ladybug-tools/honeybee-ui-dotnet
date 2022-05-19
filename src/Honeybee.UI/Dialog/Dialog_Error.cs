347564b0fde7b52e65c73607202cf6b7e91805d5
MingboPeng <mingo1214@gmail.com>
2022-05-19 20:02:12 +0800

    fix(Dialog_Error): update Dialog_Error
    Support multi-selection
    Support linking error code documentation
    Support "Re-validate"
    Support double-click to expand group item

--- src/Honeybee.UI/Dialog/Dialog_Error.cs
+++ src/Honeybee.UI/Dialog/Dialog_Error.cs
@@ -1,35 +1,56 @@
 ﻿using Eto.Forms;
+using System.Linq;
 using System;
+using System.Collections.Generic;
 
 namespace Honeybee.UI
 {
     public class Dialog_Error : Eto.Forms.Form
     {
         private ErrorViewModel _vm;
+        private static Dialog_Error _instance;
+        private TreeGridView _grid;
 
-        public Dialog_Error(HoneybeeSchema.ValidationReport report, Action<HoneybeeSchema.ValidationError, bool> showAction = default)
+        public static Dialog_Error Instance
         {
-            this.Title = $"Error - {DialogHelper.PluginName}";
+            get 
+            {
+                if (_instance == null)
+                    _instance = new Dialog_Error(null);
+                return _instance; 
+            }
+        }
+
+        public void Update(
+            HoneybeeSchema.ValidationReport report, 
+            Action<IEnumerable<HoneybeeSchema.ValidationError>, bool> showAction = default,
+            Action reRunValidation = default)
+        {
+            this._vm.Update(report, showAction, reRunValidation);
+            this._vm.UILoaded();
+            this._grid?.ReloadData();
+        }
+
+        private Dialog_Error(HoneybeeSchema.ValidationReport report)
+        {
+            this.Title = $"Validation Report - {DialogHelper.PluginName}";
             this.Width = 800;
             this.Icon = DialogHelper.HoneybeeIcon;
 
-            _vm = new ErrorViewModel(report, this, showAction);
+            _vm = new ErrorViewModel(report, this);
 
-         
-            var grid = GenGridView();
+            _grid = GenGridView();
             var nextBtn = new Button() { Text = ">>"};
+            var currentErrorIndex = new Label();
             var preBtn = new Button() { Text = "<<" };
             var showBtn = new Button() { Text = "Show" };
             var showParentBtn = new Button() { Text = "Show Parent" };
             var moreInfoBtn = new Button() { Text = "More Info" };
-
-            var totalErrorCount = new Label();
-            var currentErrorIndex = new Label();
-
+            
             var message = new TextArea() { Height = 84 };
 
+
             message.TextBinding.Bind(_vm, _=>_.CurrentErrorMessage);
-            totalErrorCount.TextBinding.Bind(_vm, _=>_.TotalErrorCount);
             currentErrorIndex.TextBinding.Bind(_vm, _=>_.CurrentErrorIndex);
             nextBtn.Bind(_ => _.Enabled, _vm, _ => _.NextBtnEnabled);
             preBtn.Bind(_ => _.Enabled, _vm, _ => _.PreBtnEnabled);
@@ -40,29 +61,28 @@ namespace Honeybee.UI
 
             nextBtn.Command = _vm.NextBtnCommand;
             preBtn.Command = _vm.PreBtnCommand;
-            moreInfoBtn.Command = _vm.MoreInfoCommand;
+            moreInfoBtn.Command = _vm.ErrorLinkCommand;
             showBtn.Command = _vm.ShowCommand;
             showParentBtn.Command = _vm.ShowParentCommand;
+            //errorLink.Command = _vm.ErrorLinkCommand;
 
-            var group = new GroupBox() { Text = $"Found {_vm.TotalErrorCount} Error(s)" };
+
+            var group = new GroupBox();
+            group.Bind(_ => _.Text, _vm, _ => _.TotalErrorMessage);
+
             group.Size = new Eto.Drawing.Size(-1, -1);
             var groupLayout = new DynamicLayout();
             groupLayout.DefaultSpacing = new Eto.Drawing.Size(5, 5);
             groupLayout.DefaultPadding = new Eto.Drawing.Padding(5);
-            groupLayout.AddSeparateRow(grid);
+            groupLayout.AddSeparateRow(_grid);
             groupLayout.AddSeparateRow(preBtn, currentErrorIndex, nextBtn, null, showBtn, showParentBtn, moreInfoBtn);
             groupLayout.AddSeparateRow(message);
             group.Content = groupLayout;
 
-            var OkBtn = new Eto.Forms.Button() { Text = "OK" };
-            OkBtn.Click += (s, e) => {
-                //if (_vm.Validate())
-                //{
-                //    var lg = _vm.GetLegend();
-                //    this.Close(lg);
-                //}
-               
-            };
+            var validateBtn = new Eto.Forms.Button() { Text = "Re-Validate" };
+            validateBtn.Bind(_=>_.Visible, _vm, _ => _.ValidateBtnEnabled);
+            validateBtn.Command = _vm.ValidateCommand;
+           
             var abortButton = new Eto.Forms.Button() { Text = "Close", Height = 24 };
             abortButton.Click += (s, e) => { this.Close(); };
 
@@ -70,7 +90,7 @@ namespace Honeybee.UI
             layout.DefaultSpacing = new Eto.Drawing.Size(5, 2);
             layout.DefaultPadding = new Eto.Drawing.Padding(4);
             layout.AddSeparateRow(controls: new[] { group }, xscale: true, yscale: true);
-            layout.AddSeparateRow(null, abortButton);
+            layout.AddSeparateRow(null, validateBtn, abortButton);
             this.Content = layout;
 
             _vm.UILoaded();
@@ -79,22 +99,32 @@ namespace Honeybee.UI
 
         public void ShowModal(Eto.Forms.Window owner)
         {
+            if (!this.Loaded)
+            {
+                this.Owner = owner;
 
-            this.Owner = owner;
-
-            var c = owner.Bounds.Center;
-            c.X = c.X - this.Width / 2;
-            c.Y = c.Y - 200;
-            this.Location = c;
+                var c = owner.Bounds.Center;
+                c.X = c.X - this.Width / 2;
+                c.Y = c.Y - 200;
+                this.Location = c;
+            }
+            
             this.Show();
+        }
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            _instance?.Dispose();
+            _instance = null;
         }
 
         private TreeGridView GenGridView()
         {
             var gd = new TreeGridView();
+            gd.AllowMultipleSelection = true;
             gd.Bind(_ => _.DataStore, _vm, _ => _.GridViewDataCollection);
-            gd.SelectedItemsChanged += (s, e) => { 
-                _vm.SelectedError = gd.SelectedItem as ErrorData;
+            gd.SelectedItemsChanged += (s, e) => {
+                _vm.SelectedErrors = gd.SelectedItems?.OfType<ErrorData>()?.ToList();
             };
 
             gd.ShowHeader = false;
@@ -106,6 +136,17 @@ namespace Honeybee.UI
                 HeaderText = "Error Message"
                 
             });
+
+            gd.CellDoubleClick += (s, e) =>
+            {
+                var data = e.Item as ErrorData;
+                if (data != null && data.IsParent)
+                {
+                    data.Expanded = !data.Expanded;
+                    gd.ReloadData();
+                }
+
+            };
 
 
             return gd;
