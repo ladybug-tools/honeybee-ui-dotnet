28007ed4b4759808d05153169c50d7377506bf1e
MingboPeng <mingo1214@gmail.com>
2022-05-19 20:02:12 +0800

    fix(Dialog_Error): update Dialog_Error
    Support multi-selection
    Support linking error code documentation
    Support "Re-validate"
    Support double-click to expand group item

--- src/Honeybee.UI/ViewModel/ErrorViewModel.cs
+++ src/Honeybee.UI/ViewModel/ErrorViewModel.cs
@@ -8,6 +8,7 @@ namespace Honeybee.UI
     public class ErrorData: TreeGridItem
     {
         public string ErrorCode { get; set; }
+        public string ErrorType => Error?.ErrorType;
         public string DisplayMessage { get; set; }
         public string DisplayFullMessage { get; set; }
         public bool IsParent { get; set; }
@@ -31,7 +32,7 @@ namespace Honeybee.UI
 
         public ErrorData(string fatalErrorMessage)
         {
-            var fatalError = new HoneybeeSchema.ValidationError("999999", HoneybeeSchema.ExtensionTypes.Core, HoneybeeSchema.ObjectTypes.Room, "FatalElementID", fatalErrorMessage);
+            var fatalError = new HoneybeeSchema.ValidationError("000000", "Fatal Error", HoneybeeSchema.ExtensionTypes.Core, HoneybeeSchema.ObjectTypes.Room, "FatalElementID", fatalErrorMessage);
             this.Error = fatalError;
             this.ErrorCode = fatalError.Code;
             this.DisplayMessage = fatalErrorMessage;
@@ -44,7 +45,8 @@ namespace Honeybee.UI
             this.Children.AddRange(children);
             var errorCount = children.Count();
 
-            this.DisplayMessage = errorCount == 1 ? $"Error Code {errorCode} (1 Error)" : $"Error Code {errorCode} ({errorCount} Errors)";
+            var errorCountMsg = errorCount == 1 ? $"1 Error" : $"{errorCount} Errors";
+            this.DisplayMessage = $"Found {errorCountMsg}: {children.FirstOrDefault()?.Error?.ErrorType} (Code {errorCode})";
             this.IsParent = true;
         }
     }
@@ -54,8 +56,20 @@ namespace Honeybee.UI
         public string TotalErrorCount
         {
             get => totalErrorCount;
-            set => Set(() => totalErrorCount = value, nameof(TotalErrorCount));
+            set
+            {
+                Set(() => totalErrorCount = value, nameof(TotalErrorCount));
+                TotalErrorMessage = $"Found {value} Error(s)";
+            }
         }
+
+        private string totalErrorMessage;
+        public string TotalErrorMessage
+        {
+            get => totalErrorMessage;
+            set => Set(() => totalErrorMessage = value, nameof(TotalErrorMessage));
+        }
+
 
         //private string fatalError;
         //public string FatalError
@@ -114,6 +128,14 @@ namespace Honeybee.UI
             set => Set(() => _moreBtnEnabled = value, nameof(MoreBtnEnabled));
         }
 
+        private bool _validateBtnEnabled;
+        public bool ValidateBtnEnabled
+        {
+            get => _validateBtnEnabled;
+            set => Set(() => _validateBtnEnabled = value, nameof(ValidateBtnEnabled));
+        }
+
+
         //private DataStoreCollection<ErrorData> _errorColletion;
 
         private TreeGridItemCollection _gridViewDataCollection;
@@ -122,6 +144,34 @@ namespace Honeybee.UI
             get => _gridViewDataCollection;
             set => this.Set(() => _gridViewDataCollection = value, nameof(_gridViewDataCollection));
         }
+
+        private List<ErrorData> _selectedErrors;
+        public List<ErrorData> SelectedErrors
+        {
+            get => _selectedErrors;
+            set 
+            {
+                this.Set(() => _selectedErrors = value, nameof(SelectedErrors));
+
+                if (_selectedErrors == null)
+                {
+                    SelectedError = null;
+                }
+                else
+                {
+                    if (_selectedErrors.Count == 1)
+                        SelectedError = _selectedErrors.FirstOrDefault();
+                    else
+                    {
+                        CurrentErrorMessage = String.Join(Environment.NewLine, _selectedErrors.Select(_ => _.DisplayFullMessage));
+                    }
+
+                   
+                }
+          
+            }
+        }
+
 
         private ErrorData _selectedError;
         public ErrorData SelectedError
@@ -145,12 +195,25 @@ namespace Honeybee.UI
         private List<ErrorData> _errors = new List<ErrorData>();
         private HoneybeeSchema.ValidationReport _report;
         private Eto.Forms.Control _control;
-        private Action<HoneybeeSchema.ValidationError, bool> _showAction;
-        public ErrorViewModel(HoneybeeSchema.ValidationReport report,  Eto.Forms.Control control, Action<HoneybeeSchema.ValidationError, bool> showAction)
+        private Action<IEnumerable<HoneybeeSchema.ValidationError>, bool> _showAction;
+        private Action _rerunValidationAction;
+        public ErrorViewModel(
+            HoneybeeSchema.ValidationReport report, 
+            Eto.Forms.Control control)
+        {
+            _control = control;
+            var rpt = report ?? new HoneybeeSchema.ValidationReport("0.0.0", "0.0.0", false);
+            Update(rpt, null, null);
+        }
+
+        public void Update(
+            HoneybeeSchema.ValidationReport report, 
+            Action<IEnumerable<HoneybeeSchema.ValidationError>, bool> showAction,
+            Action reRunValidation = default)
         {
             _report = report;
-            _control = control;
             _showAction = showAction;
+            _rerunValidationAction = reRunValidation;
 
             _errors = new List<ErrorData>();
 
@@ -166,10 +229,17 @@ namespace Honeybee.UI
 
             var errorGroups = _errors
                 .GroupBy(_ => _.ErrorCode)
-                .Select(_=> new ErrorData(_.Key, _));
+                .Select(_ => new ErrorData(_.Key, _));
 
-            GridViewDataCollection = new TreeGridItemCollection(errorGroups);
+            if (GridViewDataCollection == null)
+                GridViewDataCollection = new TreeGridItemCollection(errorGroups);
+            else
+            {
+                GridViewDataCollection.Clear();
+                GridViewDataCollection.AddRange(errorGroups);
+            }
 
+            this.ValidateBtnEnabled = reRunValidation != null;
             this.TotalErrorCount = _errors?.Count.ToString();
         }
 
@@ -183,17 +253,23 @@ namespace Honeybee.UI
 
         public void Refresh()
         {
-            if (this.SelectedError == null)
-                return;
-
             this.CurrentErrorIndex = $"{_currentIndex} of {TotalErrorCount}";
-
             this.PreBtnEnabled = _currentIndex > 1;
             this.NextBtnEnabled = _currentIndex < _errors.Count;
 
-            this.ShowBtnEnabled = SelectedError.IsGeometry && _showAction != null;
-            this.ShowParentBtnEnabled = SelectedError.HasParentGeometry && _showAction != null;
-            this.MoreBtnEnabled = SelectedError != null && SelectedError.Error != null;
+            if (this.SelectedError != null)
+            {
+                this.ShowBtnEnabled = SelectedError.IsGeometry && _showAction != null;
+                this.ShowParentBtnEnabled = SelectedError.HasParentGeometry && _showAction != null;
+                this.MoreBtnEnabled = SelectedError != null && SelectedError.Error != null;
+            }
+            else
+            {
+                this.ShowBtnEnabled = false;
+                this.ShowParentBtnEnabled = false;
+                this.MoreBtnEnabled = false;
+            }
+
         }
 
         public Eto.Forms.RelayCommand PreBtnCommand => new Eto.Forms.RelayCommand(() =>
@@ -222,32 +298,39 @@ namespace Honeybee.UI
             this.SelectedError = this._errors.ElementAt(_currentIndex - 1);
         });
 
-        public Eto.Forms.RelayCommand MoreInfoCommand => new Eto.Forms.RelayCommand(() =>
-        {
-            if (this.SelectedError == null)
-                return;
-
-            Dialog_Message.Show(this._control, this.SelectedError.Error.ToJson(true));
-        });
+      
 
         public Eto.Forms.RelayCommand ShowCommand => new Eto.Forms.RelayCommand(() =>
         {
-            if (this.SelectedError == null)
+            if (this.SelectedErrors == null)
                 return;
 
-            var err = this.SelectedError.Error;
-            _showAction?.Invoke(err, false);
+            var errs = this.SelectedErrors.Select(_ => _.Error).Where(_ => _ != null);
+            _showAction?.Invoke(errs, false);
         });
 
         public Eto.Forms.RelayCommand ShowParentCommand => new Eto.Forms.RelayCommand(() =>
         {
-            if (this.SelectedError == null)
+            if (this.SelectedErrors == null)
                 return;
 
-            var err = this.SelectedError.Error;
-            _showAction?.Invoke(err, true);
+            var errs = this.SelectedErrors.Select(_ => _.Error).Where(_ => _ != null);
+            _showAction?.Invoke(errs, true);
         });
 
+        public Eto.Forms.RelayCommand ValidateCommand => new Eto.Forms.RelayCommand(() =>
+        {
+            _rerunValidationAction?.Invoke();
+        });
+
+        public Eto.Forms.RelayCommand ErrorLinkCommand => new Eto.Forms.RelayCommand(() =>
+        {
+            //https://docs.pollination.cloud/user-manual/get-started/troubleshooting/help-with-modeling-error-codes#000204
+            var url = @"https://docs.pollination.cloud/user-manual/get-started/troubleshooting/help-with-modeling-error-codes";
+            if (SelectedError != null)
+                url = $"{url}#{SelectedError.ErrorCode}";
+            System.Diagnostics.Process.Start(url);
+        });
 
     }
 
