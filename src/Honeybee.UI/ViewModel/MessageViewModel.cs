313064a82bc44827560c755d458d8452342ee544
MingboPeng <mingo1214@gmail.com>
2022-10-14 00:46:19 -0400

    fix(Dialog_Message): improve UI with a full message area

--- src/Honeybee.UI/ViewModel/MessageViewModel.cs
+++ src/Honeybee.UI/ViewModel/MessageViewModel.cs
@@ -1,27 +1,87 @@
-﻿using System;
+﻿using Eto.Forms;
+using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
 namespace Honeybee.UI
 {
-    public class MessageViewModel : INotifyPropertyChanged
+    public class MessageViewModel : ViewModelBase
     {
+        private string _TitleText;
+        public string TitleText
+        {
+            get => string.IsNullOrEmpty(_TitleText) ? DialogHelper.PluginName : $"{_TitleText} - {DialogHelper.PluginName}";
+            set { this.Set(() => _TitleText = value, nameof(TitleText)); }
+        }
+
+        private string _InfoText;
+        public string InfoText
+        {
+            get => _InfoText;
+            set { this.Set(() => _InfoText = value, nameof(InfoText)); }
+        }
+
         private string _messageText;
         public string MessageText
         {
-            get { return _messageText; }
-            set
+            get => _messageText;
+            set { this.Set(() => _messageText = value, nameof(MessageText)); }
+        }
+
+        public bool HasMessageText => !string.IsNullOrEmpty(this.MessageText);
+
+        private string _FullMessageText;
+        public string FullMessageText
+        {
+            get => _FullMessageText;
+            set { this.Set(() => _FullMessageText = value, nameof(FullMessageText)); }
+        }
+
+        public bool HasFullMessageText => !string.IsNullOrEmpty(this.FullMessageText);
+       
+
+        private bool _ShowFullMessageText;
+        public bool ShowFullMessageText
+        {
+            get 
             {
-                if (_messageText != value)
-                {
-                    _messageText = value;
-                    OnPropertyChanged();
-                }
+                if (!HasMessageText)
+                    return true;
+                return _ShowFullMessageText;
+            }
+            set
+            { 
+                this.Set(() => _ShowFullMessageText = value, nameof(ShowFullMessageText));
+                this.DetailButtonText = value ? "Hide details": "Show details";
             }
         }
 
-        private static readonly MessageViewModel _instance = new MessageViewModel();
-        public static MessageViewModel Instance => _instance;
+        private string _DetailButtonText = "Show details";
+        public string DetailButtonText
+        {
+            get => _DetailButtonText;
+            set { this.Set(() => _DetailButtonText = value, nameof(DetailButtonText)); }
+        }
+
+        private bool _ShowDetailButton = true;
+        public bool ShowDetailButton
+        {
+            get
+            {
+                if (!HasMessageText)
+                    return false;
+                return _ShowDetailButton;
+            }
+            set { this.Set(() => _ShowDetailButton = value, nameof(ShowDetailButton)); }
+        }
+
+
+        private static MessageViewModel _instance;
+        public static MessageViewModel Instance
+        {
+            get => _instance = _instance ?? new MessageViewModel();
+        }
 
 
 
@@ -29,16 +89,37 @@ namespace Honeybee.UI
         {
         }
 
-        public void Update(string message)
+
+        public void Update(string message, string fullMessage, string title, string info)
         {
             MessageText = message;
+            FullMessageText = fullMessage; 
+            TitleText = title;
+            InfoText = info;
+
+            this.ShowFullMessageText = this.HasMessageText ? false: true;
+            this.ShowDetailButton = this.HasFullMessageText;
         }
 
-        void OnPropertyChanged([CallerMemberName] string memberName = null)
+
+        public ICommand DetailBtnClick => new RelayCommand(() =>
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(memberName));
-        }
-        public event PropertyChangedEventHandler PropertyChanged;
+            if (!this.HasFullMessageText)
+                return;
+
+            this.ShowFullMessageText = !this.ShowFullMessageText;
+
+            if (this.ShowFullMessageText)
+            {
+               
+
+            }
+            else // hide
+            {
+            }
+
+
+        });
     }
 
 
