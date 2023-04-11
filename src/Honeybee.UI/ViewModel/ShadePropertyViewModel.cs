2ca4c505edc342f1bc62812b60015fb63e7f3c4b
MingboPeng <mingo1214@gmail.com>
2023-04-11 12:16:03 +0800

    fix(DoorProperty): Glass door can only have WindowConstruction, Shade can only have ShadeConstruction

--- src/Honeybee.UI/ViewModel/ShadePropertyViewModel.cs
+++ src/Honeybee.UI/ViewModel/ShadePropertyViewModel.cs
@@ -284,7 +284,14 @@ namespace Honeybee.UI.ViewModel
             var dialog_rc = dialog.ShowModal(this._control);
             if (dialog_rc != null)
             {
-                this.Construction.SetPropetyObj(dialog_rc[0]);
+                var c = dialog_rc[0];
+                var isShadeC = c is HoneybeeSchema.Energy.IShadeConstruction;
+                if (!isShadeC)
+                {
+                    Honeybee.UI.Dialog_Message.Show($"Cannot assign {c?.GetType()?.Name} to a shade!");
+                    return;
+                }
+                this.Construction.SetPropetyObj(c);
             }
         });
         public RelayCommand ScheduleCommand => new RelayCommand(() =>
