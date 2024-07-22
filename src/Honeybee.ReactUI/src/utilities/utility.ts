67564f5fd2c45cdc4f08ab53a65777f00bd0cf7d
MingboPeng <mingo1214@gmail.com>
2024-07-22 16:58:31 -0700

    fix(React): move React to Pollination UI

--- src/Honeybee.ReactUI/src/utilities/utility.ts
+++
@@ -1,3 +0,0 @@
-export const GetEnumKeys = <T extends string, TEnumValue extends string | number>(enumVariable: { [key in T]: TEnumValue }): Array<T>=> {
-    return Object.keys(enumVariable) as Array<T>;
-  }
